693f5366f16b2eca3e6938cb18052142ab29f454
andychang <andychang@tencent.com>
2015-12-05 18:18:39 +0800

    BouncingBalls
    知识点：
    MotionEvent
    ValueAnimator
    AnimatorSet
    OvalShape
    ShapeDrawable
    ShapeHolder
    Paint
    RadialGradient
    
    RectShape

--- apidemos/src/main/java/com/example/android/apis/animation/BouncingBalls.java
+++ apidemos/src/main/java/com/example/android/apis/animation/BouncingBalls.java
@@ -18,11 +18,14 @@ package com.example.android.apis.animation;
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
-import android.annotation.TargetApi;
-import android.graphics.drawable.ColorDrawable;
-import com.example.android.apis.R;
 
-import android.animation.*;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -31,6 +34,7 @@ import android.graphics.RadialGradient;
 import android.graphics.Shader;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.RectShape;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -39,11 +43,15 @@ import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
 
+import com.example.android.apis.R;
+
 import java.util.ArrayList;
 
 
 public class BouncingBalls extends Activity {
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,52 +93,61 @@ public class BouncingBalls extends Activity {
                     event.getAction() != MotionEvent.ACTION_MOVE) {
                 return false;
             }
-            ShapeHolder newBall = addBall(event.getX(), event.getY());
+            ShapeHolder newBall = addRect(event.getX(), event.getY());
 
             // Bouncing animation with squash and stretch
             float startY = newBall.getY();
             float endY = getHeight() - 50f;
-            float h = (float)getHeight();
+            float h = (float) getHeight();
             float eventY = event.getY();
-            int duration = (int)(500 * ((h - eventY)/h));
+            int duration = (int) (500 * ((h - eventY) / h));
+
             ValueAnimator bounceAnim = ObjectAnimator.ofFloat(newBall, "y", startY, endY);
             bounceAnim.setDuration(duration);
             bounceAnim.setInterpolator(new AccelerateInterpolator());
-            ValueAnimator squashAnim1 = ObjectAnimator.ofFloat(newBall, "x", newBall.getX(),
+
+            ValueAnimator squashAnim1x = ObjectAnimator.ofFloat(newBall, "x", newBall.getX(),
                     newBall.getX() - 25f);
-            squashAnim1.setDuration(duration/4);
-            squashAnim1.setRepeatCount(1);
-            squashAnim1.setRepeatMode(ValueAnimator.REVERSE);
-            squashAnim1.setInterpolator(new DecelerateInterpolator());
-            ValueAnimator squashAnim2 = ObjectAnimator.ofFloat(newBall, "width", newBall.getWidth(),
+            squashAnim1x.setDuration(duration / 4);
+            squashAnim1x.setRepeatCount(1);
+            squashAnim1x.setRepeatMode(ValueAnimator.REVERSE);
+            squashAnim1x.setInterpolator(new DecelerateInterpolator());
+
+            ValueAnimator squashAnim2width = ObjectAnimator.ofFloat(newBall, "width", newBall.getWidth(),
                     newBall.getWidth() + 50);
-            squashAnim2.setDuration(duration/4);
-            squashAnim2.setRepeatCount(1);
-            squashAnim2.setRepeatMode(ValueAnimator.REVERSE);
-            squashAnim2.setInterpolator(new DecelerateInterpolator());
-            ValueAnimator stretchAnim1 = ObjectAnimator.ofFloat(newBall, "y", endY,
+            squashAnim2width.setDuration(duration / 4);
+            squashAnim2width.setRepeatCount(1);
+            squashAnim2width.setRepeatMode(ValueAnimator.REVERSE);
+            squashAnim2width.setInterpolator(new DecelerateInterpolator());
+
+            ValueAnimator stretchAnim1y = ObjectAnimator.ofFloat(newBall, "y", endY,
                     endY + 25f);
-            stretchAnim1.setDuration(duration/4);
-            stretchAnim1.setRepeatCount(1);
-            stretchAnim1.setInterpolator(new DecelerateInterpolator());
-            stretchAnim1.setRepeatMode(ValueAnimator.REVERSE);
-            ValueAnimator stretchAnim2 = ObjectAnimator.ofFloat(newBall, "height",
+            stretchAnim1y.setDuration(duration / 4);
+            stretchAnim1y.setRepeatCount(1);
+            stretchAnim1y.setInterpolator(new DecelerateInterpolator());
+            stretchAnim1y.setRepeatMode(ValueAnimator.REVERSE);
+
+            ValueAnimator stretchAnim2height = ObjectAnimator.ofFloat(newBall, "height",
                     newBall.getHeight(), newBall.getHeight() - 25);
-            stretchAnim2.setDuration(duration/4);
-            stretchAnim2.setRepeatCount(1);
-            stretchAnim2.setInterpolator(new DecelerateInterpolator());
-            stretchAnim2.setRepeatMode(ValueAnimator.REVERSE);
+            stretchAnim2height.setDuration(duration / 4);
+            stretchAnim2height.setRepeatCount(1);
+            stretchAnim2height.setInterpolator(new DecelerateInterpolator());
+            stretchAnim2height.setRepeatMode(ValueAnimator.REVERSE);
+
+
             ValueAnimator bounceBackAnim = ObjectAnimator.ofFloat(newBall, "y", endY,
                     startY);
             bounceBackAnim.setDuration(duration);
             bounceBackAnim.setInterpolator(new DecelerateInterpolator());
+
+
             // Sequence the down/squash&stretch/up animations
             AnimatorSet bouncer = new AnimatorSet();
-            bouncer.play(bounceAnim).before(squashAnim1);
-            bouncer.play(squashAnim1).with(squashAnim2);
-            bouncer.play(squashAnim1).with(stretchAnim1);
-            bouncer.play(squashAnim1).with(stretchAnim2);
-            bouncer.play(bounceBackAnim).after(stretchAnim2);
+            bouncer.play(bounceAnim).before(squashAnim1x);
+            bouncer.play(squashAnim1x).with(squashAnim2width).with(stretchAnim1y).with(stretchAnim2height);
+//            bouncer.play(squashAnim1x).with(stretchAnim1y);
+//            bouncer.play(squashAnim1x).with(stretchAnim2height);
+            bouncer.play(bounceBackAnim).after(stretchAnim2height);
 
             // Fading animation - remove the ball when the animation is done
             ValueAnimator fadeAnim = ObjectAnimator.ofFloat(newBall, "alpha", 1f, 0f);
@@ -138,14 +155,15 @@ public class BouncingBalls extends Activity {
             fadeAnim.addListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    balls.remove(((ObjectAnimator)animation).getTarget());
+                    balls.remove(((ObjectAnimator) animation).getTarget());
 
                 }
             });
 
             // Sequence the two animations to play one after the other
             AnimatorSet animatorSet = new AnimatorSet();
-            animatorSet.play(bouncer).before(fadeAnim);
+//            animatorSet.play(bouncer).before(fadeAnim);
+            animatorSet.play(fadeAnim).after(bouncer);
 
             // Start the animation
             animatorSet.start();
@@ -160,18 +178,39 @@ public class BouncingBalls extends Activity {
             ShapeHolder shapeHolder = new ShapeHolder(drawable);
             shapeHolder.setX(x - 25f);
             shapeHolder.setY(y - 25f);
-            int red = (int)(Math.random() * 255);
-            int green = (int)(Math.random() * 255);
-            int blue = (int)(Math.random() * 255);
+            int red = (int) (Math.random() * 255);
+            int green = (int) (Math.random() * 255);
+            int blue = (int) (Math.random() * 255);
             int color = 0xff000000 | red << 16 | green << 8 | blue;
             Paint paint = drawable.getPaint(); //new Paint(Paint.ANTI_ALIAS_FLAG);
-            int darkColor = 0xff000000 | red/4 << 16 | green/4 << 8 | blue/4;
+            int darkColor = 0xff000000 | red / 4 << 16 | green / 4 << 8 | blue / 4;
             RadialGradient gradient = new RadialGradient(37.5f, 12.5f,
                     50f, color, darkColor, Shader.TileMode.CLAMP);
             paint.setShader(gradient);
             shapeHolder.setPaint(paint);
             balls.add(shapeHolder);
             return shapeHolder;
+        }
+
+        private ShapeHolder addRect(float x, float y) {
+            RectShape rectShape = new RectShape();
+            rectShape.resize(50f, 50f);
+            ShapeDrawable drawable = new ShapeDrawable(rectShape);
+            ShapeHolder shapeHolder = new ShapeHolder(drawable);
+            shapeHolder.setX(x - 25f);
+            shapeHolder.setY(y - 25f);
+            int red = (int) (Math.random() * 255);
+            int green = (int) (Math.random() * 255);
+            int blue = (int) (Math.random() * 255);
+            int color = 0xff000000 | red << 16 | green << 8 | blue;
+            int darkColor = 0xff000000 | red / 4 << 16 | green / 4 << 8 | blue / 4;
+            Paint paint = drawable.getPaint();
+            RadialGradient gradient = new RadialGradient(37.5f, 12.5f, 50f, color, darkColor, Shader.TileMode.CLAMP);
+            paint.setShader(gradient);
+            shapeHolder.setPaint(paint);
+            balls.add(shapeHolder);
+            return shapeHolder;
+
         }
 
         @Override
